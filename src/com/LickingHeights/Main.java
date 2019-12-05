ec44ebaf7d0dffdbb654cfc2bf3e022276fe786b
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-12-05 09:44:50 -0500

    Pattern2 complete

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -12,6 +12,7 @@ public class Main {
         int[] array = createNumberArray(size, lowestNumber, highestNumber);
 
         Pattern1();
+        Pattern2();
     }
 
     public static int[] createNumberArray(int size, int lowestNumber, int highestNumber) {
@@ -44,6 +45,16 @@ public class Main {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+
+    public static void Pattern2(){
+        for(int rows = 1; rows <7 ;rows++){
+
+            for(int columns = 1; columns < 7; columns++){
+                System.out.print(rows);
+            }
+            System.out.println(rows);
         }
     }
 }
