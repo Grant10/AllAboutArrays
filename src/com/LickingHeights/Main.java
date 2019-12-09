f9feb84e521e26a12d2def12f1b92b5eb971749e
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-12-09 09:55:29 -0500

    Sum Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int size = 10;
-        int lowestNumber = 0;
-        int highestNumber = 10;
+        int lowestNumber = 80;
+        int highestNumber = 90;
 
 
         int[] array = createNumberArray(size, lowestNumber, highestNumber);
+        
+        System.out.println("The sum of the array is " + sum(array));
 
-//        Pattern1();
-//        Pattern2();
-        //Pattern3();
-        //Pattern4();
+
+        Pattern1();
+        Pattern2();
+        Pattern3();
+        Pattern4();
         Pattern5();
     }
 
@@ -41,51 +44,62 @@ public class Main {
         return number;
     }
 
-//    public static void Pattern1() {
-//        for (int rows = 0; rows < 6; rows++) {
-//
-//            for (int columns = 0; columns < 6; columns++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//    public static void Pattern2() {
-//        for (int rows = 1; rows < 7; rows++) {
-//
-//            for (int columns = 1; columns < 7; columns++) {
-//                System.out.print(rows);
-//            }
-//            System.out.println(rows);
-//        }
-//    }
+    public static void Pattern1() {
+        for (int rows = 0; rows < 6; rows++) {
 
-//    public static void Pattern3() {
-//        for (int rows = 1; rows < 7; rows++) {
-//
-//            for (int columns = 1; columns < 7; columns++) {
-//                System.out.print(columns);
-//            }
-//            System.out.println();
-//        }
-//    }
-//    public static void Pattern4() {
-//        for (int times = 1; times < 7; times++) {
-//
-//            for (int columns = 1; columns <=times; columns++) {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//    }
-    public static void Pattern5() {
-        for (int times = 1; times < 7; times++) {
+            for (int columns = 0; columns < 6; columns++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
-            for (int columns = 1; columns <=times; columns++) {
+    public static void Pattern2() {
+        for (int rows = 1; rows < 7; rows++) {
+
+            for (int columns = 1; columns < 7; columns++) {
+                System.out.print(rows);
+            }
+            System.out.println(rows);
+        }
+    }
+
+    public static void Pattern3() {
+        for (int rows = 1; rows < 7; rows++) {
+
+            for (int columns = 1; columns < 7; columns++) {
                 System.out.print(columns);
             }
             System.out.println();
         }
+    }
+
+    public static void Pattern4() {
+        for (int times = 1; times < 7; times++) {
+
+            for (int columns = 1; columns <= times; columns++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern5() {
+        for (int times = 1; times < 7; times++) {
+
+            for (int columns = 1; columns <= times; columns++) {
+                System.out.print(columns);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
     }
 }
